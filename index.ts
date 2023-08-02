4c12139f555b757d91db2bad8a19202c2c248ae6
vanimarcos <stackvany@gmail.com>
2023-08-02 13:55:45 +0100

    added a return value to the function factorial

--- index.ts
+++ index.ts
@@ -140,7 +140,7 @@ console.log(secondAddedValues(12, 10));
 // recursive function in Typescript 
 
 
-function factorial(number:number) {
+function factorial(number:number):number {
     if (number <= 0) return 1;
     return (number * factorial(number - 1))
 }
